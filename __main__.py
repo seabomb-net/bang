c89fdfe8b0dfb61480bde75ce500bedec1b87cbd
seabomb.net <inbox@seabomb.net>
2025-10-12 21:33:14 -0400

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -1,8 +1,27 @@
 import sys
 python_version = sys.version.split()[0]
 if sys.version_info < (3, 10):
-    print(f"BANG requires Python 3.10+\nYou are using Python {python_version}, which is not supported by BANG.")
-    input("Enter any key to exit...")
+    print("""
+                    .   .xXXXX+.   .
+               .   ..   xXXXX+.-   ..   .   
+         .   ..  ... ..xXXXX+. --.. ...  ..   .
+     .   ..  ... .....xXXXX+.  -.-..... ...  ..   .
+   .   ..  ... ......xXXXX+.  . .--...... ...  ..   . 
+  .   ..  ... ......xXXXX+.    -.- -...... ...  ..   .
+ .   ..  ... ......xXXXX+.   .-+-.-.-...... ...  ..   .
+ .   ..  ... .....xXXXX+. . --xx+.-.--..... ...  ..   .
+.   ..  ... .....xXXXX+. - .-xxxx+- .-- .... ...  ..   .
+ .   ..  ... ...xXXXX+.  -.-xxxxxx+ .---... ...  ..   .
+ .   ..  ... ..xXXXX+. .---..xxxxxx+-..--.. ...  ..   .
+  .   ..  ... xXXXX+. . --....xxxxxx+  -.- ...  ..   .
+   .   ..  ..xXXXX+. . .-......xxxxxx+-. --..  ..   .
+     .   .. xXXXXXXXXXXXXXXXXXXXxxxxxx+. .-- ..   .
+         . xXXXXXXXXXXXXXXXXXXXXXxxxxxx+.  -- .
+           xxxxxxxxxxxxxxxxxxxxxxxxxxxxx+.--
+            xxxxxxxxxxxxxxxxxxxxxxxxxxxxx+-\n""") # art by Ojosh!ro
+    print(f"You are running Python {python_version}.".center(56))
+    print(f"Please upgrade to 3.10 or newer to run BANG.".center(56))
+    input("Enter any key to exit...".center(56))
     sys.exit(1)
 import bang
 bang.main()
